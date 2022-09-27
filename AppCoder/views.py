e9a729f0516e91bd4ff1e151043d4c792cf16b3a
Cesar Brousset <cesarbroussetlara@gmail.com>
2022-09-27 10:48:29 -0300

    jugando con django

---
+++ AppCoder/views.py
@@ -0,0 +1,23 @@
+from django.template import Context, Template
+
+from django.http import HttpResponse
+
+import random
+import datetime
+
+def saludo(request):
+    return HttpResponse('Buenas noches!')
+
+def saludo_con_nombre(request, nombre, edad):
+    hoy = datetime.datetime.now()
+    return HttpResponse(f'Buenas noches!: {nombre} <br> tu edad es {edad}')
+
+def saludo_con_template(request):
+    mi_archivo = open('C:\ProyectoCoder\MVTCesarBrousset\AppCoder\templates\template1.html')
+    template = Template(mi_archivo.read())
+    mi_archivo.close()
+    
+    context = Context()
+    
+    res = template.render(context)
+    return HttpResponse(res)
