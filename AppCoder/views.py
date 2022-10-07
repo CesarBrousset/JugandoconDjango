e091287ed413bca98c1ccfc063583e5f348c5ee0
Cesar Brousset <cesarbroussetlara@gmail.com>
2022-10-07 12:19:39 -0300

    Ultimando detalles

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -4,5 +4,6 @@ from AppCoder.models import Estudiante
 
 def mostrar_inicio(request):
     estudiante = Estudiante(nombre = 'Cesar', email = 'cesar@appcoder.com ')
-    contexto = {'estudiante_1' : estudiante}    
+    contexto = {'estudiante_1' : estudiante}
+    estudiante.save()    
     return render(request, "AppCoder/inicio.html", contexto)
